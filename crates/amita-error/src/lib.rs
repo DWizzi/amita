002e8e95d2623c1c81a14a00c255bb8744e71cae
David LI <davidwzlee@outlook.com>
2024-08-08 16:36:45 +0800

    feat(logit): added non-binary error type for logit

--- crates/amita-error/src/lib.rs
+++ crates/amita-error/src/lib.rs
@@ -2,14 +2,14 @@
 #[derive(Debug, Clone, thiserror::Error)]
 pub enum AmitaError {
     // Matrix
-    #[error("{matrix_name:?} is expected to be a single-column, vector-like array")]
-    ExpectedVector { matrix_name: String },
     #[error("Matrices consist of a different number of observations")]
     NotSameObservations,
     #[error("{matrix_name:?} is not QR-decomposable")]
     NotQRDecomposable { matrix_name: String },
     #[error("{matrix_name:?} is not inversable")]
     NotInversable { matrix_name: String },
+    #[error("Elements in {matrix_name:?} are non-binary")]
+    NonBinary { matrix_name: String },
 
     // Solver
     #[error("Solver is not solved")]
