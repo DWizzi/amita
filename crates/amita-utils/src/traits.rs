24afb7d9d6cd309e58e1acf8be8111bfa22d2508
David LI <davidwzlee@outlook.com>
2024-08-15 19:08:48 +0800

    feat: adding new traits

--- crates/amita-utils/src/traits.rs
+++ crates/amita-utils/src/traits.rs
@@ -2,6 +2,29 @@ use ndarray::Array1;
 
 use amita_error::AmitaError;
 
+pub trait AmitaModel<R>
+where
+    Self: Sized,
+    R: AmitaModelResults,
+{
+    fn fit(&self) -> Result<R, AmitaError>;
+}
+
+pub trait AmitaModelResults {
+    fn summary(&self) -> String;
+}
+
+
+pub trait BaseSolver<R> 
+where
+    Self: Sized,
+    R: BaseResults,
+{
+    fn results(&self) -> R;
+
+    fn solve(self) -> Result<Self, AmitaError>;
+}
+
 pub trait BaseResults {
     fn coef(&self) -> Result<Array1<f64>, AmitaError>;
 
@@ -12,14 +35,4 @@ pub trait BaseResults {
     fn p_vals(&self) -> Result<Array1<f64>, AmitaError>;
 
     fn summary(&self) -> Result<String, AmitaError>;
-}
-
-pub trait BaseSolver<R> 
-where
-    Self: Sized,
-    R: BaseResults,
-{
-    fn results(&self) -> R;
-
-    fn solve(self) -> Result<Self, AmitaError>;
 }
